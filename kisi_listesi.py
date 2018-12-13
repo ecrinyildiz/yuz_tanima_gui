e1d838b656fbfdcd4cdb41cdbf8bf791272cb837
Ecrin <40597127+ecrinyildiz@users.noreply.github.com>
2018-12-13 22:27:12 +0300

    Update kisi_listesi.py

--- kisi_listesi.py
+++ kisi_listesi.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'kisi_listesi.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sqlite3
 
