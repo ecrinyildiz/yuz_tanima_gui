d6795073fe80307fd65bf6a388d6cdf2644c5b09
Ecrin <40597127+ecrinyildiz@users.noreply.github.com>
2018-12-16 19:31:35 +0300

    Update kayit_ekrani.py

--- kayit_ekrani.py
+++ kayit_ekrani.py
@@ -7,7 +7,10 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5 import QtGui, QtCore
-from modules import *
+from modules2 import *
+
+
+
 
 class Kayit_Ekrani(object):
 
@@ -27,24 +30,25 @@ class Kayit_Ekrani(object):
         self.kayit_no_label = QtWidgets.QLabel(self.centralwidget)
         self.kayit_no_label.setObjectName("kayit_no_label")
         self.gridLayout.addWidget(self.kayit_no_label, 1, 0, 1, 1)
-        self.cinsiyet_label = QtWidgets.QLabel(self.centralwidget)
-        self.cinsiyet_label.setObjectName("cinsiyet_label")
-        self.gridLayout.addWidget(self.cinsiyet_label, 3, 0, 1, 1)
+        self.kayit_no_label2 = QtWidgets.QLabel ( self.centralwidget )
+        self.kayit_no_label2.setObjectName ( "kayit_no_label2" )
+        self.gridLayout.addWidget ( self.kayit_no_label2, 1, 1, 1, 1 )
+        self.soyad_label = QtWidgets.QLabel(self.centralwidget)
+        self.soyad_label.setObjectName("soyad_label")
+        self.gridLayout.addWidget(self.soyad_label, 3, 0, 1, 1)
         self.isim_label = QtWidgets.QLabel(self.centralwidget)
         self.isim_label.setObjectName("isim_label")
         self.gridLayout.addWidget(self.isim_label, 2, 0, 1, 1)
         self.kaydet_butonu = QtWidgets.QPushButton(self.centralwidget)
         self.kaydet_butonu.setObjectName("kaydet_butonu")
         self.gridLayout.addWidget(self.kaydet_butonu, 6, 1, 1, 1)
-        self.cinsiyet_text = QtWidgets.QLineEdit(self.centralwidget)
-        self.cinsiyet_text.setObjectName("cinsiyet_text")
-        self.gridLayout.addWidget(self.cinsiyet_text, 3, 1, 1, 1)
+        self.soyad_text = QtWidgets.QLineEdit(self.centralwidget)
+        self.soyad_text.setObjectName("soyad_text")
+        self.gridLayout.addWidget(self.soyad_text, 3, 1, 1, 1)
         self.isim_text = QtWidgets.QLineEdit(self.centralwidget)
         self.isim_text.setObjectName("isim_text")
         self.gridLayout.addWidget(self.isim_text, 2, 1, 1, 1)
-        self.kayit_no_text = QtWidgets.QLineEdit(self.centralwidget)
-        self.kayit_no_text.setObjectName("kayit_no_text")
-        self.gridLayout.addWidget(self.kayit_no_text, 1, 1, 1, 1)
+
         self.cikis_butonu = QtWidgets.QPushButton(self.centralwidget)
         self.cikis_butonu.setObjectName("cikis_butonu")
         self.gridLayout.addWidget(self.cikis_butonu, 7, 1, 1, 1)
@@ -58,26 +62,26 @@ class Kayit_Ekrani(object):
         self.cikis_butonu.clicked.connect(yeni_kayit_ekrani.close)
         self.kaydet_butonu.clicked.connect(self.kaydet_gonder)
         QtCore.QMetaObject.connectSlotsByName(yeni_kayit_ekrani)
-        yeni_kayit_ekrani.setTabOrder(self.kayit_no_text, self.isim_text)
-        yeni_kayit_ekrani.setTabOrder(self.isim_text, self.cinsiyet_text)
-        yeni_kayit_ekrani.setTabOrder(self.cinsiyet_text, self.kaydet_butonu)
+        yeni_kayit_ekrani.setTabOrder(self.isim_text, self.soyad_text)
+        yeni_kayit_ekrani.setTabOrder(self.soyad_text, self.kaydet_butonu)
         yeni_kayit_ekrani.setTabOrder(self.kaydet_butonu, self.cikis_butonu)
 
 
     def kaydet_gonder(self):
-        id = int ( self.kayit_no_text.text () )
+        a = c.execute ( "select * from kayitli_kisiler" )
+        b =  ( len ( a.fetchall () ) + 1 )
         isim = self.isim_text.text ()
-        cinsiyet = self.cinsiyet_text.text ()
+        soyad = self.soyad_text.text ()
         msg = QMessageBox()
-        Bilgileriniz = "Id no: " + str ( id ) + "\nİsim: " + str ( isim ) + "\nCinsiyet: " + str ( cinsiyet )
+        Bilgileriniz = "Id no: " + str(b) + "\nİsim: " + str ( isim ) + "\nSoyisim: " + str ( soyad )
         msg.setText(Bilgileriniz)
         msg.setWindowTitle("Bilgileriniz")
         retval = msg.exec()
         retval
-        kaydet(id, isim, cinsiyet)
-        self.kayit_no_text.setText("")
+        kaydet(isim, soyad)
         self.isim_text.setText ( "" )
-        self.cinsiyet_text.setText ( "" )
+        self.soyad_text.setText ( "" )
+        print(type(b))
         msg2 = QMessageBox ()
         msg2.setWindowTitle(" ")
         msg2.setText ( "Kayıt Başarılı." )
@@ -86,12 +90,16 @@ class Kayit_Ekrani(object):
 
 
 
+
     def retranslateUi(self, yeni_kayit_ekrani):
+        a = c.execute("select * from kayitli_kisiler")
+        b = str(len(a.fetchall())+1)
         _translate = QtCore.QCoreApplication.translate
         yeni_kayit_ekrani.setWindowTitle(_translate("yeni_kayit_ekrani", "Kayıt Ekranı"))
         self.label.setText(_translate("yeni_kayit_ekrani", "Kameranın önünde hafifçe kafanızı sağa ve sola doğru çeviriniz"))
         self.kayit_no_label.setText(_translate("yeni_kayit_ekrani", "Kayıt No:"))
-        self.cinsiyet_label.setText(_translate("yeni_kayit_ekrani", "Cinsiyet:"))
+        self.kayit_no_label2.setText ( _translate ( "yeni_kayit_ekrani",b ) )
+        self.soyad_label.setText(_translate("yeni_kayit_ekrani", "Soyisim:"))
         self.isim_label.setText(_translate("yeni_kayit_ekrani", "İsim:"))
         self.kaydet_butonu.setText(_translate("yeni_kayit_ekrani", "Kaydet"))
         self.cikis_butonu.setText(_translate("yeni_kayit_ekrani", "Çıkış"))
