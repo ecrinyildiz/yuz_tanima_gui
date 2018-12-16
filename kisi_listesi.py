6d9c176d053ae09bf6123311574020affa4b0d3e
Ecrin <40597127+ecrinyildiz@users.noreply.github.com>
2018-12-16 23:22:42 +0300

    Update kisi_listesi.py

--- kisi_listesi.py
+++ kisi_listesi.py
@@ -2,7 +2,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sqlite3
-from modules2 import *
+from modules import *
 
 class Ui_kayitli_kisiler(object):
     def setupUi4(self, kayitli_kisiler):
