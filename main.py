b5681430fd7111a238e76e5bc02b8e15a66ace10
Ecrin <40597127+ecrinyildiz@users.noreply.github.com>
2018-12-16 19:48:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,185 +1,154 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+import os
+import numpy as np
+from PIL import Image
+import cv2
+import sqlite3
+import serial
+import serial.tools.list_ports
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-from kayit_ekrani import *
-from modules import *
-from arduino_baglanti import *
-from kisi_listesi import *
+ser = serial.Serial()
+ports = list(serial.tools.list_ports.comports())
+conn = sqlite3.connect("DetectedFaces.db")
+c = conn.cursor()
+c.execute ( "CREATE TABLE IF NOT EXISTS kayitli_kisiler(id int, isim text, soyad text)" )
 
-class Ui_MainWindow(object):
-    def setupUi1(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(568, 316)
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("icons/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        MainWindow.setWindowIcon(icon)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName("gridLayout")
-        self.yeni_kayit_butonu = QtWidgets.QPushButton(self.centralwidget)
-        self.yeni_kayit_butonu.setAutoDefault(False)
-        self.yeni_kayit_butonu.setObjectName("yeni_kayit_butonu")
-        self.gridLayout.addWidget(self.yeni_kayit_butonu, 0, 0, 1, 1)
-        self.arduino_baglanti_butonu = QtWidgets.QPushButton(self.centralwidget)
-        self.arduino_baglanti_butonu.setEnabled(True)
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("icons/arduino.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.arduino_baglanti_butonu.setIcon(icon1)
-        self.arduino_baglanti_butonu.setObjectName("arduino_baglanti_butonu")
-        self.gridLayout.addWidget(self.arduino_baglanti_butonu, 0, 1, 1, 1)
-        self.kisileri_gosterme_butonu = QtWidgets.QPushButton(self.centralwidget)
-        self.kisileri_gosterme_butonu.setEnabled(True)
-        self.kisileri_gosterme_butonu.setObjectName("kisileri_gosterme_butonu")
-        self.gridLayout.addWidget(self.kisileri_gosterme_butonu, 1, 1, 1, 1)
+def arduinoyu_bagla(secim):
+    if secim == "1":
+        ser.port =  '/dev/ttyUSB0'   # open serial port
+        ser.open()
+        print("'ttyUSB0' portundan bağlantı sağlandı.")
 
-        self.veri_setini_egit_butonu = QtWidgets.QPushButton(self.centralwidget)
-        self.veri_setini_egit_butonu.setObjectName("veri_setini_egit_butonu")
-        self.gridLayout.addWidget(self.veri_setini_egit_butonu, 1, 0, 1, 1)
+    elif secim == "2":
+        ser.port = "com1"
+        ser.open()
+        print("'com1' portundan bağlantı sağlandı.")
+    else:
+        print(secim)
+    return secim
 
 
-        self.yuz_tanima_butonu = QtWidgets.QPushButton(self.centralwidget)
-        self.yuz_tanima_butonu.setObjectName("yuz_tanima_butonu")
-        self.gridLayout.addWidget(self.yuz_tanima_butonu, 2, 0, 1, 1)
-        self.cikis_butonu = QtWidgets.QPushButton(self.centralwidget)
-        self.cikis_butonu.setObjectName("cikis_butonu")
-        self.gridLayout.addWidget(self.cikis_butonu, 3, 0, 1, 1)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 568, 22))
-        self.menubar.setObjectName("menubar")
-        self.menuDosya = QtWidgets.QMenu(self.menubar)
-        self.menuDosya.setObjectName("menuDosya")
-        self.menu_yardim = QtWidgets.QMenu(self.menubar)
-        self.menu_yardim.setObjectName("menu_yardim")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setEnabled(True)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-        self.bilgi_menu = QtWidgets.QAction(MainWindow)
-        icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap("icons/help.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.bilgi_menu.setIcon(icon2)
-        self.bilgi_menu.setObjectName("bilgi_menu")
-        self.yeni_kayit_menu = QtWidgets.QAction(MainWindow)
-        icon3 = QtGui.QIcon()
-        icon3.addPixmap(QtGui.QPixmap("icons/yeni_dosya.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.yeni_kayit_menu.setIcon(icon3)
-        self.yeni_kayit_menu.setObjectName("yeni_kayit_menu")
-        self.veriyi_egit_menu = QtWidgets.QAction(MainWindow)
-        icon4 = QtGui.QIcon()
-        icon4.addPixmap(QtGui.QPixmap("icons/training.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.veriyi_egit_menu.setIcon(icon4)
-        self.veriyi_egit_menu.setObjectName("veriyi_egit_menu")
-        self.cikis_menu = QtWidgets.QAction(MainWindow)
-        icon5 = QtGui.QIcon()
-        icon5.addPixmap(QtGui.QPixmap("icons/exit.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.cikis_menu.setIcon(icon5)
-        self.cikis_menu.setShortcutContext(QtCore.Qt.WidgetShortcut)
-        self.cikis_menu.setObjectName("cikis_menu")
+def kaydet(isim, soyad):
+    faceDetect = cv2.CascadeClassifier ( 'haarcascade_frontalface_default.xml' )
+    cam = cv2.VideoCapture ( 0 )
 
-        self.arduino_baglantisi = QtWidgets.QAction(MainWindow)
-        self.arduino_baglantisi.setIcon(icon1)
-        self.arduino_baglantisi.setObjectName("arduino_baglantisi")
+    c.execute ( 'select id from kayitli_kisiler' )
+    kayitli_kisi_sayisi = len ( c.fetchall () )
+
+    id_ = kayitli_kisi_sayisi + 1
+    cmd = "SELECT * FROM kayitli_kisiler WHERE id=" + str ( id_ )
+    cursor = c.execute ( cmd )
+
+    c.execute ( 'insert into kayitli_kisiler values(?,?,?)', (id_, isim, soyad) )
+    conn.commit ()
+
+    if len ( c.execute ( "select id from kayitli_kisiler where id={}".format ( int ( id_ ) ) ).fetchall () ) > 1:
+        print ( "Aynı id numarası daha önce kullanılmış. Lütfen yeni bir id no ile tekrar deneyiniz." )
+        c.execute (
+            "delete from kayitli_kisiler where rowid not in (select min(rowid) from kayitli_kisiler group by id)" )
+
+    else:
+        sampleNum = 0
+        while (True):
+            ret, img = cam.read ()
+            gray = cv2.cvtColor ( img, cv2.COLOR_BGR2GRAY )
+            faces = faceDetect.detectMultiScale ( gray, 1.3, 5 )
+            for (x, y, w, h) in faces:
+                sampleNum = sampleNum + 1
+                cv2.imwrite ( 'dataSet/Kullanici.' + str ( id_ ) + '.' + str ( sampleNum ) + '.jpg',
+                              gray [y:y + h, x:x + w] )
+                cv2.rectangle ( img, (x, y), (x + w, y + h), (0, 0, 225), 2 )
+                cv2.waitKey ( 100 )
+            cv2.imshow ( 'Yuz', img )
+            cv2.waitKey ( 1 )
+            if (sampleNum > 5):
+                break
+        cam.release ()
+        cv2.destroyAllWindows ()
+        conn.commit ()
+        print ( "kayıt başarılı olmuştur." )
 
 
-        self.menuDosya.addAction(self.yeni_kayit_menu)
-        self.menuDosya.addAction(self.veriyi_egit_menu)
-        self.menuDosya.addAction ( self.arduino_baglantisi )
-        self.menuDosya.addSeparator()
-        self.menuDosya.addAction(self.cikis_menu)
-        self.menu_yardim.addAction(self.bilgi_menu)
-        self.menubar.addAction(self.menuDosya.menuAction())
-        self.menubar.addAction(self.menu_yardim.menuAction())
-        self.retranslateUi(MainWindow)
-        self.yeni_kayit_butonu.clicked.connect(self.kayit_ekrani_ac)
-        self.veri_setini_egit_butonu.clicked.connect(self.veri_setini_egit)
-        self.yuz_tanima_butonu.clicked.connect(self.yuz_tanimayi_baslat)
-        self.arduino_baglanti_butonu.clicked.connect(self.arduino_baglantisini_baslat)
-        self.cikis_butonu.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        self.kisileri_gosterme_butonu.clicked.connect(self.kisileri_goster)
+
+def id_bul(id_):
+    id_no = str ( kaydet.__get__ ( id_, int ) )
+    id_no_split = id_no.split ( ">" ) [0]
+    id_no_split1 = id_no.split ( (">") ) [0].split ( "of " ) [1]
+    Kayit_Ekrani.retranslateUi(id_no_split1)
 
 
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        MainWindow.setTabOrder(self.yeni_kayit_butonu, self.veri_setini_egit_butonu)
-        MainWindow.setTabOrder(self.veri_setini_egit_butonu, self.yuz_tanima_butonu)
-        MainWindow.setTabOrder(self.yuz_tanima_butonu, self.cikis_butonu)
-        MainWindow.setTabOrder(self.cikis_butonu, self.arduino_baglanti_butonu)
+
+def training():
+    recognizer = cv2.face.LBPHFaceRecognizer_create ()
+    path = 'dataSet'
+
+    def getImagesWithID (path):
+        imagePaths = [os.path.join ( path, f ) for f in os.listdir ( path )]
+        faces = []
+        IDs = []
+        for imagePath in imagePaths:
+            faceImg = Image.open ( imagePath ).convert ( 'L' )
+            faceNp = np.array ( faceImg, 'uint8' )
+            ID = int ( os.path.split ( imagePath ) [-1].split ( '.' ) [1] )
+            faces.append ( faceNp )
+            IDs.append ( ID )
+            cv2.imshow ( 'egitim', faceNp )
+            cv2.waitKey ( 10 )
+        return np.array ( IDs ), faces
+
+    IDs, faces = getImagesWithID ( path )
+    recognizer.train ( faces, IDs )
+    recognizer.save ( 'trainner/egitimData.yml' )
+    cv2.destroyAllWindows ()
 
 
-    def kisileri_goster(self):
-        ui4.setupUi4(kayitli_kisiler)
-        ui4.kisilere_ekle()
-        kayitli_kisiler.show()
+def getprofile(id_):
+    cmd = "SELECT isim from kayitli_kisiler WHERE id=" + str(id_)
+    cursor = conn.execute(cmd)
+    profile = None
+    for row in cursor:
+        profile = row
+
+    cmd = "select soyad from kayitli_kisiler where id=" + str(id_)
+    cursor = conn.execute(cmd)
+    profile1 = None
+    for row in cursor:
+        profile1 = row
+    return profile, profile1
 
 
-    def kayit_ekrani_ac(self):
-        ui2.setupUi2(yeni_kayit_ekrani)
-        yeni_kayit_ekrani.show()
-        print ( "kayit ekrani acildi" )
+def detect():
+    faceDetect = cv2.CascadeClassifier ( 'haarcascade_frontalface_default.xml' )
+    cam = cv2.VideoCapture ( 0 )
+    rec = cv2.face.LBPHFaceRecognizer_create ()
+    rec.read ( 'trainner/egitimData.yml' )
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    while True:
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceDetect.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
 
+            id_, conf = rec.predict(gray [y:y + h, x:x + w])
+            profile = getprofile(id_)[0]
+            profile1 = getprofile ( id_ ) [1]
+            print(conf)
+            if conf < 50:
+                if profile is not None:
+                    if ser.isOpen () == True:
+                        ser.write ( b'1' )  # write a string
+                    cv2.rectangle ( img, (x, y), (x + w, y + h), (0, 255, 0), 2 )
+                    print ( profile )
+                    cv2.putText ( img, ("Isim: " + profile [0]), (x, y + h + 30), font, 0.8, (0, 255, 0), 2 )
+                    cv2.putText ( img, ("Soyisim: " + profile1 [0]), (x, y + h + 60), font, 0.8, (0, 255, 0), 2 )
+            elif conf > 50:
+                cv2.rectangle ( img, (x, y), (x + w, y + h), (0, 0, 225), 2 )
+                print("Sisteme Kayıtlı değil!")
+                cv2.putText ( img, "Sisteme Kayitli Degil!", (x, y + h + 30), font, 0.6, (255, 122, 122), 2 )
+                if ser.isOpen () == True:
+                    ser.write ( b'2' )  # write a string
 
-    def veri_setini_egit(self):
-        training()
-        print("veriler eğitildi.")
-
-
-    def yuz_tanimayi_baslat(self):
-        detect()
-        print("detection started.")
-
-
-    def arduino_baglantisini_baslat(self):
-        ui3.setupUi3(Arduino1)
-        Arduino1.show()
-        print("bağlantı başarılı")
-
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Yüz Tanıma Sistemi"))
-        self.yeni_kayit_butonu.setText(_translate("MainWindow", "Yeni Kayıt"))
-        self.arduino_baglanti_butonu.setText(_translate("MainWindow", "Arduino Bağlantısı"))
-        self.kisileri_gosterme_butonu.setText(_translate("MainWindow", "Kişi Listesi"))
-        self.veri_setini_egit_butonu.setText(_translate("MainWindow", "Veri Setini Eğit"))
-        self.yuz_tanima_butonu.setText(_translate("MainWindow", "Yüz Tanımayı Başlat"))
-        self.cikis_butonu.setText(_translate("MainWindow", "Çıkış"))
-        self.menuDosya.setTitle(_translate("MainWindow", "Dosya"))
-        self.menu_yardim.setTitle(_translate("MainWindow", "Yardım"))
-        self.bilgi_menu.setText(_translate("MainWindow", "Bilgi"))
-        self.bilgi_menu.setShortcut(_translate("MainWindow", "Ctrl+H"))
-        self.yeni_kayit_menu.setText(_translate("MainWindow", "Yeni Kayıt"))
-        self.yeni_kayit_menu.setShortcut(_translate("MainWindow", "Ctrl+N"))
-        self.veriyi_egit_menu.setText(_translate("MainWindow", "Veriyi Eğit"))
-        self.veriyi_egit_menu.setShortcut(_translate("MainWindow", "Ctrl+T"))
-        self.cikis_menu.setText(_translate("MainWindow", "Çıkış"))
-        self.cikis_menu.setShortcut(_translate("MainWindow", "Ctrl+Q"))
-        self.arduino_baglantisi.setText(_translate("MainWindow", "Arduino Bağlantısı"))
-        self.arduino_baglantisi.setShortcut(_translate("MainWindow", "Ctrl+A"))
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-
-
-    yeni_kayit_ekrani = QtWidgets.QMainWindow ()
-    ui2 = Kayit_Ekrani ()
-    ui2.setupUi2 ( yeni_kayit_ekrani )
-
-    Arduino1 = QtWidgets.QMainWindow ()
-    ui3 = Arduino ()
-    ui3.setupUi3 ( Arduino1 )
-
-    kayitli_kisiler = QtWidgets.QMainWindow ()
-    ui4 = Ui_kayitli_kisiler ()
-    ui4.setupUi4 ( kayitli_kisiler )
-
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi1(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
-    
+        cv2.imshow ( 'Yuz Tanimlama', img )
+        if cv2.waitKey ( 10 ) == ord ( 'q' ):
+            break
+    cam.release ()
+    cv2.destroyAllWindows ()
